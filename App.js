50679691acc726c64b83100d546524f84350e00f
Samil Akpinar <samilakpinar6@gmail.com>
2020-08-08 14:36:20 +0300

    hooks ve flatList kullanımı

--- App.js
+++ App.js
@@ -1,114 +1,131 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  Text, View, StyleSheet, TouchableOpacity, FlatList,
+  SafeAreaView, KeyboardAvoidingView,
+  Platform
 } from 'react-native';
+import { Input, Button } from './Components'
+const ListPage = () => {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const [title, setTitle] = useState('')
+  const [dsc, setDsc] = useState('')
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+  const [data, setData] = useState([])
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.dsc}>{item.dsc}</Text>
+    </View>
   );
-};
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+
+        <FlatList
+          style={{ flex: 1 }}
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          ListEmptyComponent={() => {
+            return (
+              <View style={{ alignItems: 'center', marginTop: 20}}>
+                <Text style={{ fontSize: 10}}>Herhangi bir data bulunamadı.</Text>
+              </View>
+
+            )
+          }}
+          ItemSeparatorComponent={() => {
+            return(
+              <View style={{ backgroundColor: 'gray', height: 1 }}>
+              
+              </View>
+            )
+          }}
+          ListFooterComponent={() => {
+            return(
+              <View style={{ height: 50, backgroundColor: 'gray'}}>
+
+              </View>
+            )
+          }}
+          initialNumToRender={2}
+          // inverted
+          ListHeaderComponent={() => {
+            return(
+              <View style={{ height: 50, backgroundColor: 'gray'}}>
+                <Text style={{ textAlign: 'center',alignItems: 'center',paddingTop: 12, color: 'white', fontSize: 20}} >Kişiler</Text>
+              </View>
+            )
+          }}
+        />
+
+        <View style={{
+          alignItems: 'center',
+          paddingTop: 30,
+          flex: 1
+        }}>
+
+          <Input
+            placeholder='İsim'
+            value={title}
+            onChangeText={(value) => setTitle(value)}
+          />
+
+          <Input
+            placeholder='Telefon numarası'
+            value={dsc}
+            onChangeText={(value) => setDsc(value)}
+          />
+
+          <Button
+            text={'Kişi Ekle'}
+            onPress={() => {
+              let arr = data.slice()
+              let obj = {
+                id: data.length,
+                title,
+                dsc
+              };
+              arr.push(obj)
+
+              setData(arr)
+            }}
+          />
+        </View>
+
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    marginTop: 0,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  item: {
+   // padding: 20,
+   paddingLeft:15,
+    marginVertical: 8,
+    marginHorizontal: 16,
+   // borderWidth: 0.5,
+   // borderRadius: 10,
+    borderColor: 'gray'
+
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
+  title: {
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  dsc: {
+    fontSize: 14,
+    color: 'gray'
   },
 });
 
-export default App;
+export default ListPage;
